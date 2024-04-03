4226288c603fe03092227007e1a1d6ba24fbce73
LukaSparrow <wrobel.lukasz02@gmail.com>
2024-04-03 16:56:15 +0200

    SFML config

---
+++ LukaSparrow/2D-SFML-Game/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+
+#include"SFML\Graphics.hpp"
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "C++ SFML GAME");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
