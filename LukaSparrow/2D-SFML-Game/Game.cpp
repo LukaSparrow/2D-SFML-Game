dd1259d065c8515f694c2a226cf0d0ffb6df982f
LukaSparrow <wrobel.lukasz02@gmail.com>
2024-04-04 18:32:48 +0200

    Game class and State class Window initialization and config file

---
+++ LukaSparrow/2D-SFML-Game/Game.cpp
@@ -0,0 +1,81 @@
+#include"Game.h"
+
+// Static Functions
+
+// Initializers
+void Game::initWindow()
+{
+    /* Creates a SFML window using options from a window.ini file */
+    std::ifstream ifs("Config/window.ini");
+
+    std::string title = "none";
+    sf::VideoMode window_bounds(800, 600);
+    unsigned framerate_limit = 120;
+    bool vertical_sync_enabled = false;
+
+    if (ifs.is_open()) 
+    {
+        std::getline(ifs, title);
+        ifs >> window_bounds.width >> window_bounds.height;
+        ifs >> framerate_limit;
+        ifs >> vertical_sync_enabled;
+    }
+
+    ifs.close();
+
+    this->window = new sf::RenderWindow(window_bounds, title);
+    this->window->setFramerateLimit(framerate_limit);
+    this->window->setVerticalSyncEnabled(vertical_sync_enabled);
+}
+
+// Constructors / Destructors
+Game::Game()
+{
+    this->initWindow();
+}
+
+Game::~Game()
+{
+	delete this->window;
+}
+
+void Game::updateDt()
+{
+    /* Updates the dt variable with the time that elapsed to render 1 frame */
+    this->dt = this->dtClock.restart().asSeconds();
+}
+
+// Functions
+
+void Game::updateSFMLEvents()
+{
+    while (this->window->pollEvent(this->sfEvent))
+    {
+        if (this->sfEvent.type == sf::Event::Closed)
+            this->window->close();
+    }
+}
+
+void Game::update()
+{
+    this->updateSFMLEvents();
+}
+
+void Game::render()
+{
+        this->window->clear();
+        
+        // Render components
+
+        this->window->display();
+}
+
+void Game::run()
+{
+    while (this->window->isOpen())
+    {
+        this->updateDt();
+        this->update();
+        this->render();
+    }
+}
---
+++ LukaSparrow/2D-SFML-Game/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include"State.h"
+
+class Game
+{
+private:
+
+	// Variables
+	sf::RenderWindow* window;
+	sf::Event sfEvent;
+
+	sf::Clock dtClock;
+	float dt;
+
+	// Initialization
+	void initWindow();
+
+public:
+	// Constructors / Destructors
+	Game();
+	virtual ~Game();
+
+	// Functions
+	void updateDt();
+	void updateSFMLEvents();
+	void update();
+	void render();
+	void run();
+};
